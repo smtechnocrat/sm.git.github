cc87ecd79b8b341c08c52ca917a7f3f80b3e133c
smtechnocrat <smtechnocrat@gmail.com>
2013-08-14 20:11:54 -0400

    New changes 1

---
+++ sm.git.first/src/sm/git/first/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sm.git.first;
+
+/**
+ * @author smazumder
+ * 
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+
+	}
+
+}
